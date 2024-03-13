50fb97ec757b7786f3106b740b6ac6581ee7728b
Uday Malakar <147069723+UdayMalakar@users.noreply.github.com>
2024-03-13 22:11:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const dbconnect = require("./config/database");
 const router = require("./routes/userRoutes");
 require("dotenv").config();
 const cookieParser =require("cookie-parser");
+const cors = require('cors');
 const product=require("./models/productSchema");
 const order =require("./models/orderhHisSchema")
 const PORT= process.env.PORT || 5000;
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
