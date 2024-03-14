32802bcd4925dc682d71f8896ade195d830fc1e8
Uday Malakar <147069723+UdayMalakar@users.noreply.github.com>
2024-03-14 16:41:01 +0530

    Update product.js

--- controllers/product.js
+++ controllers/product.js
@@ -116,3 +116,25 @@ exports.getAllUserProduct =async(req,res)=>{
         })
     }
 };
+
+exports.getAllProducts("/data",async(req,res)=>{
+       try{
+
+        const getData=await Product.find({});
+        return res.status(200)
+        .json({
+            success:true,
+            message:"data aa gya",
+            getData,
+        })
+
+       }catch(error)
+       {
+        console.log(error);
+        return res.status(500)
+        .json({
+            success:false,
+            message:"Somthing went wrong"
+        })
+       }
+})
