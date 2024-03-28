293a8afdef396c9d53688479a3d1df4f9a767697
UdayMalakar <147069723+UdayMalakar@users.noreply.github.com>
2024-03-28 23:27:03 +0530

    lastAgain

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {signUp,login} = require("../controllers/Auth");
 const {auth, isAdmin} =require("../middlewares/auth");
-const {addProduct, removeProduct, getAllUserProduct,createProduct,updateProduct,deleteProduct} =require("../controllers/product");
+const {addProduct, removeProduct, getAllUserProduct,createProduct,updateProduct,deleteProduct, getAllProducts} =require("../controllers/product");
 const {getOrder,orderCancel,getAllOrder,OrderDeleverd,getOrderDetailes} =require("../controllers/order");
 router.post("/signup",signUp);
 router.post("/login",login);
@@ -19,12 +19,14 @@ router.get("/baba",(req,res)=>{
 router.put("/addItem/:id",auth,addProduct);
 router.put("/removeItem/:id",auth,removeProduct);
 router.get("/getProduct",auth,getAllUserProduct);
+router.get("/data",getAllProducts)
 router.post("/getOrder",auth,getOrder);
 router.put("/cancelOrder/:oId",auth,orderCancel);
 router.put("/orderDeliverd/:oId",auth,OrderDeleverd)
 router.get("/getAllOrder",auth,getAllOrder);
 router.post("/uploadProduct",auth,isAdmin,createProduct);
 router.put("/updateProduct/:id",auth,isAdmin,updateProduct);
-router.delete("/deleteProduct/:id",auth,isAdmin,deleteProduct)
+router.delete("/deleteProduct/:id",auth,isAdmin,deleteProduct);
+
 
 module.exports=router;
